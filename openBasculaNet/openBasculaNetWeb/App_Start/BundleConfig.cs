3b7f9d31a91280c3f4edcbb2a662c40d35d1bcd8
arkadoel <fer.d.minguela@gmail.com>
2017-04-04 23:05:15 +0200

    Estilos y datepicker

--- openBasculaNet/openBasculaNetWeb/App_Start/BundleConfig.cs
+++ openBasculaNet/openBasculaNetWeb/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace openBasculaNetWeb
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -27,6 +30,27 @@ namespace openBasculaNetWeb
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
                       "~/Content/OpenBasculaNet.css"));
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/accordion.css",
+                        "~/Content/themes/base/all.css",
+                        "~/Content/themes/base/autocomplete.css",
+                        "~/Content/themes/base/base.css",
+                        "~/Content/themes/base/button.css",
+                        "~/Content/themes/base/core.css",
+                        "~/Content/themes/base/datepicker.css",
+                        "~/Content/themes/base/dialog.css",
+                        "~/Content/themes/base/draggable.css",
+                        "~/Content/themes/base/menu.css",
+                        "~/Content/themes/base/progressbar.css",
+                        "~/Content/themes/base/resizable.css",
+                        "~/Content/themes/base/selectable.css",
+                        "~/Content/themes/base/selectmenu.css",
+                        "~/Content/themes/base/slider.css",
+                        "~/Content/themes/base/sortable.css",
+                        "~/Content/themes/base/spinner.css",
+                        "~/Content/themes/base/tabs.css",
+                        "~/Content/themes/base/theme.css",
+                        "~/Content/themes/base/tooltip.css"));
         }
     }
 }
